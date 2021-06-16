8d805eb0afc76457905cd9047b4a1be22c17ea05
Filip Jirsák <filip@jirsak.org>
2021-06-16 17:52:22 +0200

    Controller pro libovolné datum.

--- src/main/java/cz/czechitas/java2webapps/lekce12/controller/SvatkyController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce12/controller/SvatkyController.java
@@ -2,20 +2,25 @@ package cz.czechitas.java2webapps.lekce12.controller;
 
 import cz.czechitas.java2webapps.lekce12.service.SvatkyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Controller pro zobrazení svátků.
  */
 @Controller
 public class SvatkyController {
+  private final SimpleDateFormat dateFormat = new SimpleDateFormat("d. MMM yyyy", new Locale("cs-CZ"));
 
   private final SvatkyService svatkyService;
 
@@ -44,6 +49,17 @@ public class SvatkyController {
   @ResponseBody
   public List<String> tomorrowJson() {
     return json(svatkyService.tomorrow());
+  }
+
+  @GetMapping(path = "/{date}")
+  public ModelAndView date(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    return html(date, dateFormat.format(date));
+  }
+
+  @GetMapping(path = "/{date}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseBody
+  public List<String> dateJson(@RequestParam("date") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
+    return json(date);
   }
 
   private ModelAndView html(LocalDate date, String textDay) {
